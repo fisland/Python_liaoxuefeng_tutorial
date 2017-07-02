0c16365b2abfcfba15ca4db53a8abe523277ac68
fisland <whereisfisland@163.com>
2017-07-03 00:32:06 +0800

    urllib请求

--- system_module.py
+++ system_module.py
@@ -185,7 +185,7 @@ class myHTMLParser(HTMLParser):
 
 # 作业，解析出时间 地址 名称
 
-
+'''
 class Python_event_HTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         def _attr(attrlist, attrname):
@@ -230,8 +230,49 @@ if __name__ == '__main__':
         parser_event.feed(htmltext)
     for i in range(len(parser_event.event_title)):
         print('time %s at location %s, the event %s' % (parser_event.event_time[i], parser_event.event_location[i],     parser_event.event_title[i]))
-
+'''
 # urllib
 # 请求 get post
+# request
+from urllib import request
 
+# with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+#     data = f.read()
+#     print('status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s:%s' % (k, v))
+#     print('Data:', data.decode('utf-8'))
 
+from urllib import parse 
+req = request.Request('http://www.douban.com/')
+req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+with request.urlopen(req) as f:
+    data = f.read()
+    print('status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s:%s' % (k, v))
+    print('Data:', data.decode('utf-8'))
+
+print('Login to weibo.cn...')
+email = input('Email: ')
+passwd = input('Password: ')
+login_data = parse.urlencode([
+    ('username', email),
+    ('password', passwd),
+    ('entry', 'mweibo'),
+    ('client_id', ''),
+    ('savestate', '1'),
+    ('ec', ''),
+    ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+])
+
+req = request.Request('https://passport.weibo.cn/sso/login')
+req.add_header('Origin', 'https://passport.weibo.cn')
+req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+
+with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', f.read().decode('utf-8'))
